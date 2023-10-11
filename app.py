80d0695dd222de5a7bdde05c694979c6524c70b1
Laís Urano <95968249+uranolais@users.noreply.github.com>
2023-10-11 10:05:51 -0300

    Video 1.2

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template, request, Response
+from flask import Flask,render_template, request
 import os
 import openai
 import dotenv
@@ -13,6 +13,11 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/chat", methods = ['POST'])
+def chat():
+    prompt = request.json['msg']
+    resposta = bot(prompt = prompt)
 
 if __name__ == "__main__":
     app.run(debug = True)
