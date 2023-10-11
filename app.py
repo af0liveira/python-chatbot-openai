e352a53b5009604c8bfa6c4e3510f9313a4f1ab7
Laís Urano <95968249+uranolais@users.noreply.github.com>
2023-10-11 10:41:27 -0300

    Video 3.4

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask,render_template, request, Response
+from flask import Flask
 import os
 import openai
 import dotenv
 from time import sleep
-import tiktoken
+from helpers import *
 
 app = Flask(__name__)
 app.secret_key = 'alura'
@@ -12,37 +12,7 @@ dotenv.load_dotenv()
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-def carrega(nome_do_arquivo):
-    try:
-        with open(nome_do_arquivo, "r") as arquivo:
-            dados = arquivo.read()
-            return dados
-    except IOError as e:
-        print(f"Erro no carregamento de arquivo: {e}")
-
-def salva(nome_do_arquivo, conteudo):
-    try:
-        with open(nome_do_arquivo, "a", encoding="utf-8") as arquivo:
-            arquivo.write(conteudo)
-    except IOError as e:
-        print(f"Erro ao salvar arquivo: {e}")
-
-def conta_tokens(prompt):
-    codificador = tiktoken.encoding_for_model("gpt-3.5-turbo")
-    lista_de_tokens = codificador.encode(prompt)
-    contagem = len(lista_de_tokens)
-    return contagem
-
-def limita_historico(historico,limite_maximo_de_tokens):
-    total_de_tokens = 0
-    historico_parcial = ''
-    for linha in reversed(historico.split('\n')):
-        tokens_da_linha = conta_tokens(linha)
-        total_de_tokens = total_de_tokens + tokens_da_linha
-        if (total_de_tokens > limite_maximo_de_tokens):
-            break
-        historico_parcial = linha + historico_parcial
-    return historico_parcial
+from views import *
 
 dados_ecommerce = carrega('dados_ecommerce.txt')
 def bot(prompt,historico):
@@ -84,45 +54,6 @@ def bot(prompt,historico):
                 return "Erro no GPT3: %s" % erro
             print('Erro de comunicação com OpenAI:', erro)
             sleep(1)
-
-@app.route("/")
-def home():
-    return render_template("index.html")
-
-@app.route("/chat", methods = ['POST'])
-def chat():
-    prompt = request.json['msg']
-    nome_do_arquivo = 'historico_ecomart'
-    historico = ''
-    if os.path.exists(nome_do_arquivo):
-        historico = carrega(nome_do_arquivo)
-    return Response(trata_resposta(prompt,historico,nome_do_arquivo), mimetype = 'text/event-stream')
-
-def trata_resposta(prompt,historico,nome_do_arquivo):
-    resposta_parcial = ''
-    limite_maximo_de_tokens = 2000
-    historico_parcial = limita_historico(historico,limite_maximo_de_tokens)
-    for resposta in bot(prompt,historico_parcial):
-        pedaco_da_resposta = resposta.choices[0].delta.get('content','')
-        if len(pedaco_da_resposta):
-            resposta_parcial += pedaco_da_resposta
-            yield pedaco_da_resposta 
-    conteudo = f"""
-    Historico: {historico_parcial}
-    Usuário: {prompt}
-    IA: {resposta_parcial}    
-    """
-    salva(nome_do_arquivo,conteudo)
-
-@app.route('/limparhistorico', methods = ['POST'])
-def limpar_historico():
-    nome_do_arquivo = 'historico_ecomart'
-    if os.path.exists(nome_do_arquivo):
-        os.remove(nome_do_arquivo)
-        print("Arquivo removido!")
-    else: 
-        print("Não foi possivel remover esse arquivo")
-    return {}
 
 if __name__ == "__main__":
     app.run(debug = True)
