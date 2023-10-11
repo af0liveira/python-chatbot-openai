7b1d224a78408b080e41f61cf855cb873d1540b9
Laís Urano <95968249+uranolais@users.noreply.github.com>
2023-10-11 10:08:40 -0300

    Video 1.3

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask,render_template, request
 import os
 import openai
 import dotenv
+from time import sleep
 
 app = Flask(__name__)
 app.secret_key = 'alura'
@@ -9,6 +10,41 @@ app.secret_key = 'alura'
 dotenv.load_dotenv()
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
+
+def bot(prompt):
+    maxima_repeticao = 1
+    repeticao = 0
+    while True:
+        try:
+            model='gpt-3.5-turbo'
+            prompt_do_sistema = f"""
+            Você é um chatbot de atendimento a clientes de um e-commerce.
+            Você não deve responder perguntas que não sejam dados do ecommerce informado!
+            """
+            response = openai.ChatCompletion.create(
+                messages=[
+                    {
+                        "role": "system",
+                        "content": prompt_do_sistema
+                    },
+                    {
+                        "role": "user",
+                        "content": prompt
+                    }
+                ],
+                temperature=1,
+                max_tokens=256,
+                top_p=1,
+                frequency_penalty=0,
+                presence_penalty=0,
+                model = model)
+            return response
+        except Exception as erro:
+            repeticao += 1
+            if repeticao >= maxima_repeticao:
+                return "Erro no GPT3: %s" % erro
+            print('Erro de comunicação com OpenAI:', erro)
+            sleep(1)
 
 @app.route("/")
 def home():
@@ -18,6 +54,8 @@ def home():
 def chat():
     prompt = request.json['msg']
     resposta = bot(prompt = prompt)
-
+    texto_da_resposta = resposta.choices[0].message.content
+    return texto_da_resposta
+    
 if __name__ == "__main__":
     app.run(debug = True)
